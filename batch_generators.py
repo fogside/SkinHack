0eb235f5b77cd14a06554038a317e95b5a4036d0
Denis Kuzminykh <reaper.of.midnight@gmail.com>
2016-11-11 18:23:34 +0300

    Added augmentor

--- batch_generators.py
+++ batch_generators.py
@@ -3,6 +3,8 @@ from __future__ import division, print_function
 import numpy as np
 import os
 import matplotlib.image as mpimg
+from utils import _start_shell
+from keras.preprocessing.image import ImageDataGenerator
 
 
 class AgeGenderBatchGenerator(object):
@@ -179,6 +181,75 @@ class SegmentationBatchGenerator(object):
         return batch_X
 
 
+class UnsupervisedBatchGenerator(object):
+    """
+    Generates batches of prepared images without any labels.
+    """
+    def __init__(self, X, batch_size, patch_shape, stride, random_offset):
+        """
+        :param X: python list or numpy array of images.
+        Each image should be a numpy array of shape (height, width, channels)
+        :param batch_size: number of samples returned by single call.
+        :param patch_shape: tuple, shape of patches sampled from the images (height, width)
+        :param stride: tuple, stride step sizes (vertical, horizontal)
+        :param random_offset: tuple, maximum values of random initial offsets (vertical, horizontal)
+        """
+        self.X = X
+        self.batch_size = batch_size
+
+        self.index = 0
+        self.offset = np.random.randint(random_offset[1])
+        self.i = np.random.randint(random_offset[0])
+        self.j = self.offset
+
+        self.num_images = len(X)
+
+        self.patch_shape = patch_shape
+
+        self.stride = stride
+        self.random_offset = random_offset
+
+        self.image = self.X[self.index]
+
+    def get_supervised_batch(self):
+        raise NotImplementedError()
+
+    def get_unsupervised_batch(self):
+        """
+        :return: A batch of data as a numpy array
+        """
+        batch_patches = []
+
+        for i in range(self.batch_size):
+
+            patch = self.image[self.i:self.i + self.patch_shape[0], self.j:self.j + self.patch_shape[1]]
+            batch_patches.append(np.expand_dims(patch, 0))
+
+            self.j += self.stride[1]
+
+            if self.j >= self.image.shape[1] - self.patch_shape[1]:
+
+                self.i += self.stride[0]
+                self.j = self.offset
+
+                if self.i >= self.image.shape[0] - self.patch_shape[0]:
+
+                    self.i = np.random.randint(self.random_offset[0])
+                    self.offset = np.random.randint(self.random_offset[1])
+                    self.j = self.offset
+
+                    self.index += 1
+
+                    if self.index >= self.num_images:
+                        self.index = 0
+                        order = np.random.permutation(self.X.shape[0])
+                        self.X = self.X[order]
+
+                    self.image = self.X[self.index]
+
+        return np.concatenate(batch_patches)
+
+
 class ImageFolderReader(object):
     """
     Cyclic reader of folders of images.
@@ -209,3 +280,49 @@ class ImageFolderReader(object):
 
     def read_all(self):
         return self.read(len(self.files_list))
+
+
+class Augmentor(object):
+    def __init__(self, rotation_range=30, width_shift_range=0.2, height_shift_range=0.2, shear_range=0.2,
+                 zoom_range=0.2, horizontal_flip=True, fill_mode='nearest'):
+
+        self.image_augmentor = ImageDataGenerator(
+            rotation_range=rotation_range,
+            width_shift_range=width_shift_range,
+            height_shift_range=height_shift_range,
+            shear_range=shear_range,
+            zoom_range=zoom_range,
+            horizontal_flip=horizontal_flip,
+            fill_mode=fill_mode)
+
+    def __call__(self, image_batch, mask_batch=None):
+        state = np.random.get_state()
+
+        aug_image_batch = np.array(image_batch)
+        for i in range(image_batch.shape[0]):
+            aug_image_batch[i] = self.image_augmentor.random_transform(image_batch[i])
+
+        if mask_batch is not None:
+            np.random.set_state(state)
+            aug_mask_batch = np.array(mask_batch)
+            for i in range(mask_batch.shape[0]):
+                aug_mask_batch[i] = self.image_augmentor.random_transform(mask_batch[i])
+
+            return aug_image_batch, aug_mask_batch
+        else:
+            return aug_image_batch
+
+
+"""
+Example^
+
+reader = ImageFolderReader('data/first_data')
+import matplotlib.pyplot as plt
+
+gen = UnsupervisedBatchGenerator(reader.read_all(), 100, (200, 200), (160, 160), (100, 100))
+aug = Augmentor()
+x = aug(gen.get_unsupervised_batch())
+
+plt.imshow(x[5])
+plt.show()
+"""
