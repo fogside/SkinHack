ee3127dfc0df20a3741c1bc598be697e4ff78bed
crimsonlander <d3nnis@inbox.ru>
2016-11-13 00:26:34 +0300

    triplet_pretrain fix

--- triplet_pretrain.py
+++ triplet_pretrain.py
@@ -1,14 +1,5 @@
 import tensorflow as tf
 import tensorflow.contrib.layers as layers
-import os
-from batch_generators import TripleBatchGenerator
-
-summaries_dir = '/tmp/TensorBoard/summaries/triplet_pretrain'
-
-with tf.variable_scope('inputs'):
-    X0 = tf.placeholder(dtype=tf.float32, shape=(None, 128, 128, 3), name='X0')
-    X1 = tf.placeholder(dtype=tf.float32, shape=(None, 128, 128, 3), name='X1')
-    X2 = tf.placeholder(dtype=tf.float32, shape=(None, 128, 128, 3), name='X2')
 
 
 def conv_block(X, size, reuse=None, scope=None):
@@ -36,29 +27,3 @@ def triplet_cnn(X, reuse=None, scope=None):
         X = layers.fully_connected(X, 256, reuse=reuse, scope='fully_connected')
         X = layers.fully_connected(X, 128, reuse=reuse, scope='output', activation_fn=tf.identity)
         return X
-
-y0 = triplet_cnn(X0, reuse=False)
-y1 = triplet_cnn(X1, reuse=True)
-y2 = triplet_cnn(X2, reuse=True)
-
-loss = tf.nn.l2_loss(y0 - y1) - tf.nn.l2_loss(y0 - y2)
-
-try:
-    os.rmdir(summaries_dir)
-except:
-    pass
-
-try:
-    os.makedirs(summaries_dir)
-except:
-    pass
-
-train_writer = tf.train.SummaryWriter(summaries_dir + '/train')
-generator = TripleBatchGenerator()
-
-num_steps = 10000
-
-with tf.Session() as sess:
-    tf.initialize_all_variables().run()
-    train_writer.add_graph(tf.get_default_graph())
-    train_writer.close()
