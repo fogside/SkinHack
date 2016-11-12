b321b11d06e9cb56e3ee26a8643d46af776b0733
Vladislav <waryak2012@mail.ru>
2016-11-12 14:02:49 +0300

    edited description

--- Mask_applying.py
+++ Mask_applying.py
@@ -1,7 +1,7 @@
 def Mask(batch, color_thresholds=[160, 70, 70]):  # batch(batch_size, height, width, n_channels)
     """
     Calculates a binary mask of the marked area. If the marker wasn't clear enough, borders may be interpolated.
-    :return: An 2-D array
+    :return: An 4-D array of shape (batch_size, height, width, n_channels)
     """
     red, green, blue = batch[:, :, :, 0], batch[:, :, :, 1], batch[:, :, :, 2]
     mask = (red > color_thresholds[0]) & (green < color_thresholds[1]) & (blue < color_thresholds[2])
