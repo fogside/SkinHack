eea54ce889a41deb1d44e2679cc8ea822946a8ff
crimsonlander <d3nnis@inbox.ru>
2016-11-13 11:01:41 +0300

    age regression

--- batch_generators.py
+++ batch_generators.py
@@ -898,7 +898,7 @@ class AgeFolderReader(object):
         imgs_list = list(map(mpimg.imread, file_names))
         for i in range(n):
             if imgs_list[i].shape != (1506, 2258, 3):
-                imresize(imgs_list[i], (1506, 2258))
+                imgs_list[i] = imresize(imgs_list[i], (1506, 2258))
             imgs_list[i] = np.expand_dims(imgs_list[i], 0)
 
         for i in range(n):
